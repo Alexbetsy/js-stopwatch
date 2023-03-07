4aed6080f9bfd17e51710876d26e749db34911c4
Alexandre Betsy <alexandre.betsy97@gmail.com>
2023-03-07 16:01:59 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  let min = 0;
+  let seconds = 0;
+  let tens = 0;
+
+  let appendMinutes = document.querySelector('#minutes');
+  let appendTens = document.querySelector('#tens');
+  let appendSeconds = document.querySelector('#seconds');
+
+  let startBtn = document.querySelector('#start');
+  let stopBtn = document.querySelector('#stop');
+  let resetBtn = document.querySelector('#reset');
+
+  let
+}
